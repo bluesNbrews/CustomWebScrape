8df2d0e89efc249fd7ce688eb1adf28786a78715
Steven <mstevenwilliams@gmail.com>
2019-06-19 17:34:39 -0400

    First Splash request for usproxy site that uses Javascript for pagination.

---
+++ js_website/js_website/spiders/usproxy.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy_splash import SplashRequest
+
+class UsproxySpider(scrapy.Spider):
+    name = 'usproxy'
+    #allowed_domains = ['us-proxy.org']
+    #start_urls = ['http://us-proxy.org/']
+
+    def start_requests(self):
+        url = 'https://us-proxy.org'
+        yield SplashRequest(url=url, callback=self.parse, endpoint='render.html', args={'wait' : 0.5})
+
+    def parse(self, response):
+        for tr in response.xpath("//table[@id='proxylisttable']/tbody/tr"):
+            yield{
+                'ip': tr.xpath(".//td[1]/text()").extract_first(),
+                'port': tr.xpath(".//td[2]/text()").extract_first()
+            }
