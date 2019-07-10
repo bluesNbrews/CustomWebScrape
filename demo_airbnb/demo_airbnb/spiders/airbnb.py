c60379e2c489f6216641c75cc1441d94f3c8d250
Steven <mstevenwilliams@gmail.com>
2019-07-10 16:47:31 -0400

    Scrape specific restaurant info from Airbnb. Still need a work around for 429 server response error (no ouput).

--- demo_airbnb/demo_airbnb/spiders/airbnb.py
+++ demo_airbnb/demo_airbnb/spiders/airbnb.py
@@ -11,8 +11,25 @@ class AirbnbSpider(scrapy.Spider):
 
     def parse_id(self, response):
         data = json.loads(response.body)
-        with open('sample.json', 'w') as file:
-            file.write(json.dumps(data))
+        restaurants = data.get('explore_tabs')[0].get('sections')[0].get('recommendation_items')
+        for restaurant in restaurants:
+            yield scrapy.Request(url='https://www.airbnb.com/api/v2/place_activities/{0}?key=d306zoyjsyarp7ifhu67rjxn52tv0t20&currency=USD&locale=en&_format=for_spa_activity_pdp_web'.format(restaurant.get('id')), callback=self.parse)
 
     def parse(self, response):
-        pass
+        restaurant = json.loads(response.body).get('place_activity')
+
+        yield{
+            'id': restaurant.get('id'),
+            'title': restaurant.get('title'),
+            'type': restaurant.get('action_kicker'),
+            'description': restaurant.get('description'),
+            'place': {
+                'address': restaurant.get('place').get('address'),
+                'city': restaurant.get('place').get('city'),
+                'country': restaurant.get('place').get('country'),
+                'latitude': restaurant.get('place').get('lat'),
+                'longitude': restaurant.get('place').get('lng')
+            },
+            'phone_number': restaurant.get('place').get('phone'),
+            'website': restaurant.get('place').get('website')
+        }
